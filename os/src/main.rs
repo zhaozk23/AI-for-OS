5c420f2b6adad3ee32701d0ea68cd2a0cbd64bf2
Youjie Zheng <Azure_stars@126.com>
2025-09-19 17:57:17 +0800

    Init the ch2

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,39 +1,54 @@
 //! The main module and entrypoint
 //!
-//! The operating system and app also starts in this module. Kernel code starts
+//! Various facilities of the kernels are implemented as submodules. The most
+//! important ones are:
+//!
+//! - [`trap`]: Handles all cases of switching from userspace to the kernel
+//! - [`syscall`]: System call handling and implementation
+//!
+//! The operating system also starts in this module. Kernel code starts
 //! executing from `entry.asm`, after which [`rust_main()`] is called to
-//! initialize various pieces of functionality [`clear_bss()`]. (See its source code for
+//! initialize various pieces of functionality. (See its source code for
 //! details.)
 //!
-//! We then call [`println!`] to display `Hello, world!`.
+//! We then call [`batch::run_next_app()`] and for the first time go to
+//! userspace.
 
 #![deny(missing_docs)]
 #![deny(warnings)]
 #![no_std]
 #![no_main]
 #![feature(panic_info_message)]
+#[macro_use]
+extern crate log;
 
 use core::arch::global_asm;
-use log::*;
-
-#[macro_use]
-mod console;
-mod lang_items;
-mod logging;
-mod sbi;
-
 #[path = "boards/qemu.rs"]
 mod board;
+use log::*;
+#[macro_use]
+mod console;
+pub mod batch;
+pub mod lang_items;
+pub mod logging;
+pub mod sbi;
+pub mod sync;
+pub mod syscall;
+pub mod trap;
 
 global_asm!(include_str!("entry.asm"));
+global_asm!(include_str!("link_app.S"));
 
 /// clear BSS segment
-pub fn clear_bss() {
+fn clear_bss() {
     extern "C" {
         fn sbss();
         fn ebss();
     }
-    (sbss as usize..ebss as usize).for_each(|a| unsafe { (a as *mut u8).write_volatile(0) });
+    unsafe {
+        core::slice::from_raw_parts_mut(sbss as usize as *mut u8, ebss as usize - sbss as usize)
+            .fill(0);
+    }
 }
 
 /// the rust entry-point of os
@@ -72,8 +87,7 @@ pub fn rust_main() -> ! {
         boot_stack_top as usize, boot_stack_lower_bound as usize
     );
     error!("[kernel] .bss [{:#x}, {:#x})", sbss as usize, ebss as usize);
-
-    use crate::board::QEMUExit;
-    crate::board::QEMU_EXIT_HANDLE.exit_success(); // CI autotest success
-                                                   //crate::board::QEMU_EXIT_HANDLE.exit_failure(); // CI autoest failed
+    trap::init();
+    batch::init();
+    batch::run_next_app();
 }
